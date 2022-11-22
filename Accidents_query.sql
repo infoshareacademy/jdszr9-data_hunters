abf138ec3a0a9f405f3e346ef4a0837f596cffe5
Paulina Konecka <paulinakonecka20@gmail.com>
2022-11-22 17:33:34 +0100

    Dodanie zapytań z podzialem na lata

--- Accidents_query.sql
+++ Accidents_query.sql
@@ -1,7 +1,56 @@
 
-DELETE FROM accidents WHERE 1=1;
+CREATE TABLE Accidents (
+	id varchar(55) PRIMARY key,
+	severity integer NULL,
+	start_time timestamp NULL,
+	end_time timestamp NULL,
+	start_lat real NULL,
+	start_lng real NULL,
+	end_lat real NULL,
+	end_lng real NULL,
+	distance_ml real NULL,
+	description varchar(1999) NULL,
+	numbers real NULL,
+	street varchar(70) NULL,
+	side varchar(55) NULL,
+	city varchar(55) NULL,
+	county varchar(55) NULL,
+	state varchar(55) NULL,
+	zipcode varchar(50) NULL,
+	country varchar(60) NULL,
+	timezone varchar(60) NULL,
+	airport_code varchar(60) NULL,
+	weather_timestamp timestamp NULL,
+	temperature_f real NULL,
+	wind_chill_f real NULL,
+	humidity_per real NULL,
+	pressure_in real NULL,
+	visibility_mi real NULL,
+	wind_direction varchar(55) NULL,
+	wind_speed_mph real NULL,
+	precipitation_in real NULL,
+	weather_condition varchar(55) NULL,
+	amenity boolean NULL,
+	bump boolean NULL,
+	crossing boolean NULL,
+	give_way boolean NULL,
+	junction boolean NULL,
+	no_exit boolean NULL,
+	railway boolean NULL,
+	roundabout boolean NULL,
+	station boolean NULL,
+	stop boolean NULL,
+	traffic_calming boolean NULL,
+	traffic_signal boolean NULL,
+	turning_loop boolean NULL,
+	sunrise_sunset varchar(55) NULL,
+	civil_twilight varchar(55) NULL,
+	nautical_twilight varchar(55) NULL,
+	astronomical_twilight varchar(55) NULL
+);
 
-SELECT count(*) FROM accidents a;   /* wczytanych jest 2845339 wierszy  */
+
+SELECT count(*) FROM accidents a;   /* wczytanych jest 2845342 wierszy  */
 
 SELECT * FROM accidents a  /* pogląd widoku danych */
 LIMIT 10;
@@ -47,4 +96,31 @@ ORDER BY ilosc DESC;
 SELECT sunrise_sunset , count(id) AS ilosc FROM accidents a  /* sunrise_sunset a wypadki */
 GROUP BY  sunrise_sunset  
 ORDER BY ilosc DESC;
+
+SELECT visibility_mi  , count(id) AS ilosc
+FROM accidents a  /* widoczność  a wypadki */
+GROUP BY  visibility_mi  
+ORDER BY ilosc DESC;
+
+
+CREATE VIEW rozklad_lat AS /* tworzę widok, w którym dodaje dwie kolumny, rok i miesiąc, w którym dany wypadek się rozpoczął */
+SELECT
+id, 
+to_char(start_time, 'YYYY') AS rok,
+to_char(start_time, 'MM') AS miesiac 
+FROM accidents a 
+
+DROP VIEW rozklad_lat
+
+
+SELECT count(id) AS ilosc, rok /* sprawdzam ilość wypadków w danym roku */
+FROM rozklad_lat
+GROUP BY rok 
+ORDER BY rok 
+
+SELECT count(id) AS ilosc, rok, miesiac /* ilość wypadków w danym roku w podziale na miesiące przez co będzie można sprawdzić czy jest może jakaś sezonowośc danych*/
+FROM rozklad_lat
+GROUP BY rok, miesiac
+ORDER BY rok, miesiac
+
 
